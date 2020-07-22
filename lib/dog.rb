cea90df3ba4cf6cdde81d62dd62f9fdb3103dd9e
john beltrez <beltrezjohn@gmail.com>
2020-07-22 05:21:33 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog 
+  def name=(dog_name)  #setter all setter properties are written like this
+    this_dogs_name = dog_name 
+  end 
+  def name     #getter
+    this_dogs_name
+  end 
+end 
